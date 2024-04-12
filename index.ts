44c099986d9065cb5f99a59691ba49f42faa36f2
Marcel Dwi Astika <marceldwias@gmail.com>
2024-04-12 20:52:36 +0700

    first init

---
+++ index.ts
@@ -0,0 +1,6 @@
+const first_number: number = 20
+const second_number: number = 30
+const sum = (first: number, second: number): string => {
+  return `${first} + ${second} = ${first + second}`
+}
+console.log(sum(first_number, second_number))
