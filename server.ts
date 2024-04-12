22a6a1ed409f5be5504b4b57aff35f6001e105bf
Marcel Dwi Astika <marceldwias@gmail.com>
2024-04-12 21:28:08 +0700

    update: add route at server

--- server.ts
+++ server.ts
@@ -1,11 +1,31 @@
-import type { Server } from 'bun'
 import figlet from 'figlet'
 
-const server: Server = Bun.serve({
+const server = Bun.serve({
   port: 3000,
   fetch(req): Response {
-    const body = figlet.textSync(`Hello World! I'm learning bun at ${req.url}`)
-    return new Response(body)
+    const url = new URL(req.url)
+    if (url.pathname === '/') {
+      const body = figlet.textSync(
+        `Hello World! I'm learning bun at ${req.url}`,
+      )
+      return new Response(body, { status: 200 })
+    }
+    if (url.pathname === '/about') {
+      return new Response('About page', { status: 200 })
+    }
+    if (url.pathname === '/contact') {
+      return new Response('Contact page', { status: 200 })
+    }
+    if (url.pathname === '/services') {
+      return new Response('Services page', { status: 200 })
+    }
+    if (url.pathname === '/products') {
+      return new Response('Products page', { status: 200 })
+    }
+    if (url.pathname === '/blog') {
+      return new Response('Blog page', { status: 200 })
+    }
+    return new Response('Not Found', { status: 404 })
   },
 })
 console.log(`Server is running on port ${server.port}!`)
